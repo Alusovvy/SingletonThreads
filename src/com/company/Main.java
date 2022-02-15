7a90ced3e44dc7297b8e906b99c488d5b1fdc58b
Alusovvy <aleksanderzawisza258@gmail.com>
2022-02-15 13:40:26 +0100

    Playing with singleton non-thread-safety code

---
+++ src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Main {
+
+    public static class SingletonThread implements Runnable {
+
+        @Override
+        public void run() {
+            Singleton singleton = Singleton.getInstance(String.valueOf("Yesss"));
+            System.out.println(singleton.value);
+            System.out.println(singleton);
+        }
+    }
+
+    public static class SingletonThread2 implements Runnable {
+
+        @Override
+        public void run() {
+            Singleton singleton = Singleton.getInstance(String.valueOf("Nooooo"));
+            System.out.println(singleton.value);
+            System.out.println(singleton);
+        }
+    }
+
+
+
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+
+        Thread t1 = new Thread(new SingletonThread());
+        Thread t2 = new Thread(new SingletonThread2());
+        t1.start();
+        t2.start();
+    }
+}
